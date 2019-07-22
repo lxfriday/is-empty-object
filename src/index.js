a78799c00f2edb865d33bc2c6f3a532b9e55a164
lxfriday <liu32481184446@outlook.com>
2019-07-22 10:52:43 +0800

    添加String、Reflect.ownKeys写法

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 /**
  * 判断一个变量是不是空对象
+ * @link
  * @param obj {any}
  */
 function isEmptyObj(obj) {
+  return String(obj) === '[object Object]' && Reflect.ownKeys(obj).length === 0
+}
+
+function isEmptyObj1(obj) {
   return (
     Object.prototype.toString.call(obj) === '[object Object]' &&
     Object.getOwnPropertyNames(obj).length === 0 &&
@@ -10,5 +15,17 @@ function isEmptyObj(obj) {
   )
 }
 
+function isEmptyObj2(obj) {
+  return (
+    obj !== null &&
+    typeof obj === 'object' &&
+    !Array.isArray(obj) &&
+    Object.getOwnPropertyNames(obj).length === 0 &&
+    Object.getOwnPropertySymbols(obj).length === 0
+  )
+}
 
 module.exports = isEmptyObj
+exports = module.exports
+exports.isEmptyObj1 = isEmptyObj1
+exports.isEmptyObj2 = isEmptyObj2
--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 const isEmptyObj = require('..')
+const { isEmptyObj1, isEmptyObj2 } = require('..')
+
 
 console.log('()', isEmptyObj())
 console.log('{}', isEmptyObj({}))
@@ -21,3 +23,19 @@ console.log('obj2', isEmptyObj(obj2))
 // { a: 1 } false
 // obj1 false
 // obj2 false
+
+console.log('------------------------------------')
+
+console.log('()', isEmptyObj1())
+console.log('{}', isEmptyObj1({}))
+console.log('{ a: 1 }', isEmptyObj1({ a: 1 }))
+console.log('obj1', isEmptyObj1(obj1))
+console.log('obj2', isEmptyObj1(obj2))
+
+console.log('------------------------------------')
+
+console.log('()', isEmptyObj2())
+console.log('{}', isEmptyObj2({}))
+console.log('{ a: 1 }', isEmptyObj2({ a: 1 }))
+console.log('obj1', isEmptyObj2(obj1))
+console.log('obj2', isEmptyObj2(obj2))
